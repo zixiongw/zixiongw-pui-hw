53b6e2b5d94e230f355a526931d94a457f698583
Zixiong Wei <zixiongw@andrew.cmu.edu>
2022-10-04 02:32:04 -0400

    hw3 dropdown js setup

--- solution-hw3/js/script.js
+++ solution-hw3/js/script.js
@@ -1,4 +1,40 @@
+// Creating arrays of options for select elements
+
+const allGlazings = ['Keep original', 'Sugar milk', 'Vanilla milk', 'Double Chocolate'];
+const allPackSizes = [1, 3, 6, 12];
+
+function populateOptions(selectElement, optionArray) {
+    for (let i = 0; i < optionArray.length; i++) {
+        let option = document.createElement('option');
+        option.text = optionArray[i];
+        option.value = optionArray[i]; 
+        selectElement.add(option);
+    }
+}
+
+// Populating dropdown fields with created arrays 
+
+let dropdown1 = document.querySelector('#glazing');
+let dropdown2 = document.querySelector('#pack_size');
+
+populateOptions(dropdown1, allGlazings);
+populateOptions(dropdown2, allPackSizes);
 
 
 
+const basePrice = 2.49;
+let glazingPrice = 0.5;
+let packSize = 1;
 
+function updatePrice(price) {
+    let price_rounded = price.toFixed(2);
+    let realtimePriceElement = document.querySelector('#realtime_price');
+    realtimePriceElement.innerText = '$ ' + String(price_rounded);
+}
+
+// function glazingChange()
+
+// function packSizeChange()
+
+let finalPrice = (basePrice + glazingPrice) * packSize;
+updatePrice(4.99);
